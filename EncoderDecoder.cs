86f7cc7f39dba4440e269aac070dd832b12c9f1a
Sander Buruma <sander.buruma@horganizer.com>
2020-03-10 13:24:00 +0100

    reformat with 3space indentation and extended the key. Rolled out the prime calculator a bit.

--- EncoderDecoder.cs
+++ EncoderDecoder.cs
@@ -7,64 +7,64 @@ using System.Threading.Tasks;
 namespace XorEncoding
 {
 
-    static class EncoderDecoder
-    {
-        public static readonly String CharsString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .";
-        /// <summary>
-        /// Converts chars to a custom char array
-        /// </summary>
-        /// <param name="that">The string to be encoded. May only contain characters found directly on the MacBook pro.</param>
-        /// <returns></returns>
-        private static Int32[] ToEncodedIntArr(this String that)
-        {
-            List<Int32> rtn = new List<Int32> { };
-            Char[] thatChars = that.ToCharArray();
-            foreach (var c in thatChars)
-            {
-                rtn.Add(CharsString.IndexOf(c));
-            }
+   static class EncoderDecoder
+   {
+      public static readonly String CharsString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .";
+      /// <summary>
+      /// Converts chars to a custom char array
+      /// </summary>
+      /// <param name="that">The string to be encoded. May only contain characters found directly on the MacBook pro.</param>
+      /// <returns></returns>
+      private static Int32[] ToEncodedIntArr(this String that)
+      {
+         List<Int32> rtn = new List<Int32> { };
+         Char[] thatChars = that.ToCharArray();
+         foreach (var c in thatChars)
+         {
+            rtn.Add(CharsString.IndexOf(c));
+         }
 
-            return rtn.ToArray();
-        }
+         return rtn.ToArray();
+      }
 
-        /// <summary>
-        /// This encodes when first run and decodes when run again on its own result with the same key and repeats
-        /// </summary>
-        /// <param name="msg">The message to be en/decoded</param>
-        /// <param name="key">The encyrption key</param>
-        /// <param name="repeats">The number of repitions to go through to encode it</param>
-        /// <returns></returns>
-        public static String EnOrDecode(this String msg, String key, Int32 repeats = 3)
-        {
-            if (repeats % 2 == 0) repeats++;
-            Int32[] msgInts = msg.ToEncodedIntArr();
-            Int32[] keyInts = key.ToEncodedIntArr();
-            Int32 maxLen = Math.Max(msg.Length, key.Length);
-            for (Int32 i = 0; i < maxLen * repeats; i++)
-            {
-                msgInts[i % msgInts.Length] = msgInts[i % msgInts.Length] ^ keyInts[i % keyInts.Length];
-            }
+      /// <summary>
+      /// This encodes when first run and decodes when run again on its own result with the same key and repeats
+      /// </summary>
+      /// <param name="msg">The message to be en/decoded</param>
+      /// <param name="key">The encyrption key</param>
+      /// <param name="repeats">The number of repitions to go through to encode it</param>
+      /// <returns></returns>
+      public static String EnOrDecode(this String msg, String key, Int32 repeats = 3)
+      {
+         if (repeats % 2 == 0) repeats++;
+         Int32[] msgInts = msg.ToEncodedIntArr();
+         Int32[] keyInts = key.ToEncodedIntArr();
+         Int32 maxLen = Math.Max(msg.Length, key.Length);
+         for (Int32 i = 0; i < maxLen * repeats; i++)
+         {
+            msgInts[i % msgInts.Length] = msgInts[i % msgInts.Length] ^ keyInts[i % keyInts.Length];
+         }
 
-            //convert back to string
-            StringBuilder strBldr = new StringBuilder(msg.Length);
-            for (Int32 i = 0; i < msgInts.Length; i++)
-            {
-                strBldr.Append(CharsString[msgInts[i]]);
-            }
-            return strBldr.ToString();
-        }
-        /// <summary>
-        /// Returns true if the string contains only the characters in its own hardcoded Chars string
-        /// </summary>
-        /// <param name="that">The string to check</param>
-        /// <returns></returns>
-        public static Boolean CheckStr(this string that)
-        {
-            foreach (var @char in that.ToCharArray())
-            {
-                if (CharsString.IndexOf(@char) == -1) return false;
-            }
-            return true;
-        }
-    }
+         //convert back to string
+         StringBuilder strBldr = new StringBuilder(msg.Length);
+         for (Int32 i = 0; i < msgInts.Length; i++)
+         {
+            strBldr.Append(CharsString[msgInts[i]]);
+         }
+         return strBldr.ToString();
+      }
+      /// <summary>
+      /// Returns true if the string contains only the characters in its own hardcoded Chars string
+      /// </summary>
+      /// <param name="that">The string to check</param>
+      /// <returns></returns>
+      public static Boolean CheckStr(this string that)
+      {
+         foreach (var @char in that.ToCharArray())
+         {
+            if (CharsString.IndexOf(@char) == -1) return false;
+         }
+         return true;
+      }
+   }
 }
--- Program.cs
+++ Program.cs
@@ -7,150 +7,171 @@ using XorEncoding;
 
 namespace XorEncoding
 {
-    class Program
-    {
-        enum Colors
-        {
-            Standard = ConsoleColor.White,
-            Output = ConsoleColor.Green,
-            Warning = ConsoleColor.Magenta,
-            Input = ConsoleColor.Red,
-        };
+   class Program
+   {
+      enum Colors
+      {
+         Standard = ConsoleColor.White,
+         Output = ConsoleColor.Green,
+         Warning = ConsoleColor.Magenta,
+         Input = ConsoleColor.Red,
+      };
 
       enum Config
       {
          MaxKeyLength = 1033,
       }
 
-        [STAThread]
-        static void Main()
-        {
-            Console.ForegroundColor = (ConsoleColor)Colors.Standard;
-            while (true)
+      [STAThread]
+      static void Main()
+      {
+         Console.ForegroundColor = (ConsoleColor)Colors.Standard;
+         while (true)
+         {
+            Console.WriteLine("\n");
+            String msg = GetConsoleInput("Input text to be encoded: ");
+            if (String.IsNullOrWhiteSpace(msg))
             {
-                Console.WriteLine("\n");
-                String msg = GetConsoleInput("Input text to be encoded: ");
-                if (String.IsNullOrWhiteSpace(msg))
-                {
-                    WarningToConsole("Please input a message with some content next time");
-                    continue;
-                }
-                if (!msg.CheckStr())
-                {
-                    WarningToConsole("Please use only legal characters:", true);
-                    OutputToConsole(EncoderDecoder.CharsString);
-                    continue;
-                }
-                String key = GetConsoleInput("Encryption Key: ");
-                if (String.IsNullOrWhiteSpace(key))
-                {
-                    WarningToConsole("Please input a key with some content next time");
-                    continue;
-                }
-                if (!key.CheckStr())
-                {
-                    WarningToConsole("Please use only legal characters:", true);
-                    OutputToConsole(EncoderDecoder.CharsString);
-                    continue;
-                }
-                if (key.Length > (Int32)Config.MaxKeyLength)
-                {
-                    WarningToConsole($"Please make the key less than {(Int32)Config.MaxKeyLength} chars long");
-                    continue;
-                }
-
-
-                //concat the key to increase its length if it is too short. Our presumed inputkey is shorter than 119
-                key += "M0ZMEy5ZcVNjcR8PMBiM9zVqsDqCNXD6k0YJ95uK5m1GVOWOq7MibY0See4hoB4jw8i9eZqwwISET24hdjsrC5moo2RhbpFaVM08Kd2JLMtxLDDZznZxawqB7g7votGLlERRfl2XvlCksnsd3Nv1RKe4GizXFgi7ktyybKXqXC8wXDn9np8qo0Yvl0omFUh1BgIZj8WozaBTPZXnsluFvmDuuoUAz4KyUOo9S6wllu3335iCi2q6uFou9PslyTcEqnjDXVwVWmS7QWMntLUfevZD36txiQFpCUAfpjS8rzleMXFMFzS7rEIdVO6AYCNpswbKxH7NEwHsLLBtzdHOmXf6FJc8vc3ZzKdhdyaf6sbdgtxa0ohxy8H0uGHAUbWgfbvj2xNTK3l2QOW9MhettSATPYEly6sjLRpSyS4eg6I4ZH0d5L6MAI4y0YrPwKVrjTRJvXTRnYb726kApQYXm7zHG79e75KsGFBpJJsxVuxBxJyC8cyviTzgr0XYeSyZ87tXx1UTQfD6I5ARCxpalw3bq4YHa0Wxf9sJZfCHRA5JBkla3Lu7mUJvl9OZjucZzyRa50mkwdP9Qjnefv21PdzSrzI8RxP7Z4uSedUmZYdINnz7sP5EgjuOFcLoRy0IK0AbquHw8dS1OKK20ERxTSx2jzZBM4hq5fDFmTBPriQ8BdLmWT87PGS7NrIvwTT42wFu1FlSHwsItjVbZeV1xYEt9wx2ww0LWtQRtm85kwZfpDUCcqysr7NDGgD4fP4SbBDRX85OTijJV8MFRreV17Uds9bIS2T7TfgI3nOeUV8vRXah69IO0n1MMUQrLeiV6CE8oTRHrgToHk7MYodDMvFlVpOJpluE5a61VHLQXJUfPLAQhwAVFt72KD0FT2wJK8RPD5mczyIwNykkkdUPptEAwghD4REilS24CKtBwMVq7Ra63Ebs2ZJtoEIIlPVtVWeQ1D6qJaR2QufeesuGk9GtdPWeDm4PODWfEOXPC9jxFWrv7KDkpYqIfYraUdjTnHpqdCuts0QnMUYgGj29ZjYguU3NW2FsdBH5l4g2J9QP4bEJDH5J5PkwgPLYQ8AaqGz4k2j0jFgMET0CgGbAt2OeJFBQuUhypVF7ZBGizg10lSlth7Z9mvyij9NQFo85AfMLD5fSKLw2l52fAFFbWqjO4ehGBEHJDIw0uu7EeH5UvbmfmpwVqx4doZDN07ePR7ijhDCgTieygXDnAkDnZEbiWGuOmecsIUgmsLQ5pxS74n1wroFWrTWEOIw0pn9n".Substring(0, (Int32)Config.MaxKeyLength - key.Length);
-                if (!key.CheckStr()) {
-                    throw new FormatException("Please warn the developer that his key appending string contains illegal characters");
-                }
-                //ensure that the key is a prime number of inequal length to the message
-                //without this step there is no guarantee that a different number of repeats won't result in different outcomes
-                if (key.Length == msg.Length || key.Length > (Int32)Config.MaxKeyLength) key = key.Substring(0, (Int32)Config.MaxKeyLength-2);
-
-                //prepare primes for the next step
-                List<Int32> primes = new List<Int32> { 2 };
-                for (Int32 i = 3; primes.Count() < 1000; i += 2)
-                {
-                    foreach (Int32 prime in primes)
-                    {
-                        if (i % prime == 0) break;
-                        if (prime * prime > i)
-                        {
-                            primes.Add(i);
-                            break;
-                        }
-                    }
-                }
-
-                //try to ensure that the number of repeats changes practically every time with any slight change of key value, no matter how slight
-                Int32 repeats = 1;
-                for (Int32 i = 0; i < key.Length; i++)
-                {
-                    for (Int32 j = 0; j < key[i]; j++)
-                    {
-                        repeats *= primes[i];
-                        repeats %= 0b1_00000000_00000000;
-                        if (repeats < 1) repeats = 1;
-                    }
-                }
-                repeats += 0b11100000_00000000;
-                String encoded = msg.EnOrDecode(key, repeats);
-
-                OutputToConsole("Encryption Repeats: ", repeats.ToString("N0"));
-                Console.Write("Encoded string: \"");
-                OutputToConsole(encoded, true);
-                Console.Write("\".\ntype 'y' to copy to clipboard:");
-                if (Console.ReadKey().KeyChar == 'y') System.Windows.Clipboard.SetText(encoded);
+               WarningToConsole("Please input a message with some content next time");
+               continue;
             }
-        }
+            if (!msg.CheckStr())
+            {
+               WarningToConsole("Please use only legal characters:", true);
+               OutputToConsole(EncoderDecoder.CharsString);
+               continue;
+            }
+            String key = GetConsoleInput("Encryption Key: ");
+            if (String.IsNullOrWhiteSpace(key))
+            {
+               WarningToConsole("Please input a key with some content next time");
+               continue;
+            }
+            if (!key.CheckStr())
+            {
+               WarningToConsole("Please use only legal characters:", true);
+               OutputToConsole(EncoderDecoder.CharsString);
+               continue;
+            }
+            if (key.Length > (Int32)Config.MaxKeyLength)
+            {
+               WarningToConsole($"Please make the key less than {(Int32)Config.MaxKeyLength} chars long");
+               continue;
+            }
 
-        /// <summary>
-        /// Gets String input from the user
-        /// </summary>
-        /// <param name="msg">The message to prompt the user with</param>
-        /// <returns></returns>
-        static String GetConsoleInput(String msg = "")
-        {
-            Console.ForegroundColor = (ConsoleColor)Colors.Standard;
-            Console.Write(msg);
-            Console.ForegroundColor = (ConsoleColor)Colors.Input;
-            String input = Console.ReadLine();
-            Console.ForegroundColor = (ConsoleColor)Colors.Standard;
 
-            return input;
-        }
-        /// <summary>
-        /// Show a colored message to the user
-        /// </summary>
-        /// <param name="info">The message to show to the user</param>
-        /// <param name="noLine">Whether or not following console writes should appear on a new line</param>
-        /// <returns></returns>
-        static void OutputToConsole(String info, Boolean noLine = false)
-        {
-            Console.ForegroundColor = (ConsoleColor)Colors.Output;
-            if (noLine) Console.Write(info);
-            else Console.WriteLine(info);
-            Console.ForegroundColor = (ConsoleColor)Colors.Standard;
-        }
-        /// <summary>
-        /// Show a colored message to the user, giving standard coloring to the msg parameter.
-        /// </summary>
-        /// <param name="msg">The message to show to the user in standard coloring</param>
-        /// <param name="info">The information to be provided</param>
-        /// <param name="noLine">Whether or not a later message should appear on a new line</param>
-        static void OutputToConsole(String msg, String info, Boolean noLine = false)
-        {
-            Console.Write(msg);
-            OutputToConsole(info, noLine);
-        }
-        static void WarningToConsole(String msg, Boolean noLine = false)
-        {
-            Console.ForegroundColor = (ConsoleColor)Colors.Warning;
-            if (noLine) Console.Write(msg);
-            else Console.WriteLine(msg);
-            Console.ForegroundColor = (ConsoleColor)Colors.Standard;
-        }
-    }
+            //concat the key to increase its length if it is too short. Our presumed inputkey is shorter than 119
+            key += "M0ZMEy5ZcVNjcR8PMBiM9zVqsDqCNXD6k0YJ95uK5m1GVOWOq7MibY0See4hoB4jw8i9eZqwwISET24hdjsrC5moo2RhbpFaVM08Kd2JLMtxLDDZznZxawqB7g7votGLlERRfl2XvlCksnsd3Nv1RKe4GizXFgi7ktyybKXqXC8wXDn9np8qo0Yvl0omFUh1BgIZj8WozaBTPZXnsluFvmDuuoUAz4KyUOo9S6wllu3335iCi2q6uFou9PslyTcEqnjDXVwVWmS7QWMntLUfevZD36txiQFpCUAfpjS8rzleMXFMFzS7rEIdVO6AYCNpswbKxH7NEwHsLLBtzdHOmXf6FJc8vc3ZzKdhdyaf6sbdgtxa0ohxy8H0uGHAUbWgfbvj2xNTK3l2QOW9MhettSATPYEly6sjLRpSyS4eg6I4ZH0d5L6MAI4y0YrPwKVrjTRJvXTRnYb726kApQYXm7zHG79e75KsGFBpJJsxVuxBxJyC8cyviTzgr0XYeSyZ87tXx1UTQfD6I5ARCxpalw3bq4YHa0Wxf9sJZfCHRA5JBkla3Lu7mUJvl9OZjucZzyRa50mkwdP9Qjnefv21PdzSrzI8RxP7Z4uSedUmZYdINnz7sP5EgjuOFcLoRy0IK0AbquHw8dS1OKK20ERxTSx2jzZBM4hq5fDFmTBPriQ8BdLmWT87PGS7NrIvwTT42wFu1FlSHwsItjVbZeV1xYEt9wx2ww0LWtQRtm85kwZfpDUCcqysr7NDGgD4fP4SbBDRX85OTijJV8MFRreV17Uds9bIS2T7TfgI3nOeUV8vRXah69IO0n1MMUQrLeiV6CE8oTRHrgToHk7MYodDMvFlVpOJpluE5a61VHLQXJUfPLAQhwAVFt72KD0FT2wJK8RPD5mczyIwNykkkdUPptEAwghD4REilS24CKtBwMVq7Ra63Ebs2ZJtoEIIlPVtVWeQ1D6qJaR2QufeesuGk9GtdPWeDm4PODWfEOXPC9jxFWrv7KDkpYqIfYraUdjTnHpqdCuts0QnMUYgGj29ZjYguU3NW2FsdBH5l4g2J9QP4bEJDH5J5PkwgPLYQ8AaqGz4k2j0jFgMET0CgGbAt2OeJFBQuUhypVF7ZBGizg10lSlth7Z9mvyij9NQFo85AfMLD5fSKLw2l52fAFFbWqjO4ehGBEHJDIw0uu7EeH5UvbmfmpwVqx4doZDN07ePR7ijhDCgTieygXDnAkDnZEbiWGuOmecsIUgmsLQ5pxS74n1wroFWrTWEOIw0pn9n".Substring(0, (Int32)Config.MaxKeyLength - key.Length);
+            if (!key.CheckStr())
+            {
+               throw new FormatException("Please warn the developer that his key appending string contains illegal characters");
+            }
+            //ensure that the key is a prime number of inequal length to the message
+            //without this step there is no guarantee that a different number of repeats won't result in different outcomes
+            if (key.Length == msg.Length || key.Length > (Int32)Config.MaxKeyLength) key = key.Substring(0, (Int32)Config.MaxKeyLength - 2);
+
+            //prepare primes for the next step
+            List<Int32> primes = new List<Int32> { 2 };
+            for (Int32 i = 3; primes.Count() < (Int32)Config.MaxKeyLength; i += 2)
+            {
+               foreach (Int32 prime in primes)
+               {
+                  if (i % prime == 0) break;
+                  if (prime * prime > i)
+                  {
+                     primes.Add(i);
+                     break;
+                  }
+               }
+               i += 2;
+               foreach (Int32 prime in primes)
+               {
+                  if (i % prime == 0) break;
+                  if (prime * prime > i)
+                  {
+                     primes.Add(i);
+                     break;
+                  }
+               }
+               i += 2;
+               foreach (Int32 prime in primes)
+               {
+                  if (i % prime == 0) break;
+                  if (prime * prime > i)
+                  {
+                     primes.Add(i);
+                     break;
+                  }
+               }
+            }
+
+            //try to ensure that the number of repeats changes practically every time with any slight change of key value, no matter how slight
+            Int32 repeats = 1;
+            for (Int32 i = 0; i < key.Length; i++)
+            {
+               for (Int32 j = 0; j < key[i]; j++)
+               {
+                  repeats *= primes[i];
+                  repeats %= 0b100_0000_0000_0000;
+                  if (repeats < 1) repeats = 1;
+               }
+            }
+            repeats += 0b11_00000_0000_0000;
+            String encoded = msg.EnOrDecode(key, repeats);
+
+            OutputToConsole("Encryption Repeats: ", repeats.ToString("N0"));
+            Console.Write("Encoded string: \"");
+            OutputToConsole(encoded, true);
+            Console.Write("\".\ntype 'y' to copy to clipboard:");
+            if (Console.ReadKey().KeyChar == 'y') System.Windows.Clipboard.SetText(encoded);
+         }
+      }
+
+      /// <summary>
+      /// Gets String input from the user
+      /// </summary>
+      /// <param name="msg">The message to prompt the user with</param>
+      /// <returns></returns>
+      static String GetConsoleInput(String msg = "")
+      {
+         Console.ForegroundColor = (ConsoleColor)Colors.Standard;
+         Console.Write(msg);
+         Console.ForegroundColor = (ConsoleColor)Colors.Input;
+         String input = Console.ReadLine();
+         Console.ForegroundColor = (ConsoleColor)Colors.Standard;
+
+         return input;
+      }
+      /// <summary>
+      /// Show a colored message to the user
+      /// </summary>
+      /// <param name="info">The message to show to the user</param>
+      /// <param name="noLine">Whether or not following console writes should appear on a new line</param>
+      /// <returns></returns>
+      static void OutputToConsole(String info, Boolean noLine = false)
+      {
+         Console.ForegroundColor = (ConsoleColor)Colors.Output;
+         if (noLine) Console.Write(info);
+         else Console.WriteLine(info);
+         Console.ForegroundColor = (ConsoleColor)Colors.Standard;
+      }
+      /// <summary>
+      /// Show a colored message to the user, giving standard coloring to the msg parameter.
+      /// </summary>
+      /// <param name="msg">The message to show to the user in standard coloring</param>
+      /// <param name="info">The information to be provided</param>
+      /// <param name="noLine">Whether or not a later message should appear on a new line</param>
+      static void OutputToConsole(String msg, String info, Boolean noLine = false)
+      {
+         Console.Write(msg);
+         OutputToConsole(info, noLine);
+      }
+      static void WarningToConsole(String msg, Boolean noLine = false)
+      {
+         Console.ForegroundColor = (ConsoleColor)Colors.Warning;
+         if (noLine) Console.Write(msg);
+         else Console.WriteLine(msg);
+         Console.ForegroundColor = (ConsoleColor)Colors.Standard;
+      }
+   }
 }
